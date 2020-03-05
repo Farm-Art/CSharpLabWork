9e73ff739a907d069b3ffa80e6d1c3f7ff54923f
farm-art <farmart@protonmail.com>
2020-03-05 22:40:59 +0300

    update menu to use Enum.TryParse;

--- Lab 1/Menu.cs
+++ Lab 1/Menu.cs
@@ -11,10 +11,21 @@ namespace Lab_1
             Add = 1, Remove, Print, Sort, Search, Help, Quit
         }
 
+        // Most used words for bool values
+        private enum Confirmation
+        {
+            No = 0, N = 0, False = 0,
+            Yes = 1, Y = 1, True = 1
+        }
+
         // Static variables for search functions
         private static string _modelSample, _materialSample;
         private static decimal _priceSample;
         private static bool _printerSample, _matched;
+        
+        // Static variables for run control
+        private static bool _running;
+        private static readonly List<ComputerTable> _list = new List<ComputerTable>();
         
         // Dict of sorting lambdas for each field
         private static Dictionary<ComputerTable.Key, Comparison<ComputerTable>>
@@ -44,6 +55,7 @@ namespace Lab_1
                     (t) => PrintIfTrue(t.Material == _materialSample, t)
             };
 
+        // Helper method for search functions
         private static void PrintIfTrue(bool cond, object item)
         {
             if (!cond) return;
@@ -51,71 +63,102 @@ namespace Lab_1
             _matched = true;
         }
 
-        public static void AddTable(List<ComputerTable> list)
+        // Dict of menu options
+        private static Dictionary<Menu.Item, Action>
+            _options = new Dictionary<Item, Action>
+            {
+                [Item.Add] = AddTable,
+                [Item.Remove] = RemoveTable,
+                [Item.Print] = PrintList,
+                [Item.Sort] = SortList,
+                [Item.Search] = SearchList,
+                [Item.Help] = PrintMenu,
+                [Item.Quit] = () => _running = false
+            };
+
+        public static void Run()
+        {
+            _running = true;
+            while (_running)
+            {
+                _options[GetInput()]();
+                Console.WriteLine(new string('-', 30));
+            }
+            
+        }
+
+        public static void AddTable()
         {
             Console.WriteLine("Enter new table model:");
+            Console.Write(">>> ");
             var model = Console.ReadLine();
 
-            bool hasPrinter;
-            Console.WriteLine("Does the table have a printer stand? (true/false)");
+            Console.WriteLine("Does the table have a printer stand?");
             // while (!Type.TryParse(input, out variable)) {} is the trivial way to keep
             // asking for input until the user provides a correct value.
-            while (!Boolean.TryParse(Console.ReadLine(), out hasPrinter))
-                Console.WriteLine("Failed to read value, try again.");
+            var hasPrinter = GetConfirmation();
 
             decimal price;
             Console.WriteLine("Enter the price of the table:");
+            Console.Write(">>> ");
             while (!Decimal.TryParse(Console.ReadLine(), out price))
+            {
                 Console.WriteLine("Failed to read value, try again.");
+                Console.Write(">>> ");
+            }
 
             Console.WriteLine("Enter the material this table is made of:");
+            Console.Write(">>> ");
             var material = Console.ReadLine();
 
-            list.Add(new ComputerTable(model, hasPrinter, price, material));
+            var table = new ComputerTable(model, hasPrinter, price, material);
+            _list.Add(table);
+            Console.WriteLine($"Added {table}.");
         }
 
-        public static void RemoveTable(List<ComputerTable> list)
+        public static void RemoveTable()
         {
-            if (list.IsEmpty)
+            if (_list.IsEmpty)
             {
                 Console.WriteLine("The list is empty.");
                 return;
             }
             
             Console.WriteLine("Enter index of table to remove from list:");
+            Console.Write(">>> ");
             int index = 0;
-            while (!Int32.TryParse(Console.ReadLine(), out index) || !list.IsValidIndex(index-1))
+            while (!Int32.TryParse(Console.ReadLine(), out index) || !_list.IsValidIndex(index-1))
+            {
                 Console.WriteLine("The value entered is not an integer or is out of range, try again.");
-
-            list.Remove(index-1);
+                Console.Write(">>> ");
+            }
+            _list.Remove(index-1);
         }
 
-        public static void PrintList(List<ComputerTable> list)
+        public static void PrintList()
         {
             Console.WriteLine(new string('=', 30));
-            if (list.IsEmpty)
+            if (_list.IsEmpty)
                 Console.WriteLine("The list is empty.");
             else
-                for (int i = 0; i < list.Length; i++)
-                    Console.WriteLine($"{i + 1}: {list[i]}");
+                for (int i = 0; i < _list.Length; i++)
+                    Console.WriteLine($"{i + 1}: {_list[i]}");
             Console.WriteLine(new string('=', 30));
         }
 
-        public static void SortList(List<ComputerTable> list)
+        public static void SortList()
         {
             Console.WriteLine("What criteria would you like to sort the list by?");
             Console.WriteLine("Choose one of the following: model (0), printer (1), price (2), material (3).");
             ComputerTable.Key key = GetKey();
-            
-            bool reverse;
-            Console.WriteLine("Would you like to reverse the sort? (true/false)");
-            while (!Boolean.TryParse(Console.ReadLine(), out reverse))
-                Console.WriteLine("Failed to read input. Try again.");
 
-            list.Sort(_sortFuncs[key], reverse);
+            Console.WriteLine("Would you like to reverse the sort?");
+            bool reverse = GetConfirmation();
+
+            _list.Sort(_sortFuncs[key], reverse);
         }
 
-        public static void SearchList(List<ComputerTable> list)
+        public static void SearchList()
         {
             _matched = false;
             Console.WriteLine("What field would you like to search the list by?");
@@ -127,23 +170,28 @@ namespace Lab_1
             switch (key)
             {
                 case ComputerTable.Key.Model :
+                    Console.Write(">>> ");
                     _modelSample = Console.ReadLine();
                     break;
                 case ComputerTable.Key.Printer :
-                    while (!Boolean.TryParse(Console.ReadLine(), out _printerSample))
-                        Console.WriteLine("Failed to read input, try again.");
+                    _printerSample = GetConfirmation();
                     break;
                 case ComputerTable.Key.Price :
+                    Console.Write(">>> ");
                     while (!Decimal.TryParse(Console.ReadLine(), out _priceSample))
+                    {
                         Console.WriteLine("Failed to read input, try again.");
+                        Console.Write(">>> ");
+                    }
                     break;
                 case ComputerTable.Key.Material :
+                    Console.Write(">>> ");
                     _materialSample = Console.ReadLine();
                     break;
             }
             
             // Apply func to each item
-            list.Iter(_searchFuncs[key]);
+            _list.Iter(_searchFuncs[key]);
             if (!_matched)
                 Console.WriteLine("No matches found!");
         }
@@ -152,34 +200,63 @@ namespace Lab_1
         {
             Console.WriteLine(new string('=', 30));
             Console.WriteLine("What would you like to do?");
-            Console.WriteLine("1. Add item to list;");
-            Console.WriteLine("2. Remove item from list;");
-            Console.WriteLine("3. Print the list;");
-            Console.WriteLine("4. Sort the list;");
-            Console.WriteLine("5. Search the list;");
-            Console.WriteLine("6. Print menu;");
-            Console.WriteLine("7. Quit.");
+            Console.WriteLine("1. ADD an item to the list;");
+            Console.WriteLine("2. REMOVE an item from the list;");
+            Console.WriteLine("3. PRINT the list;");
+            Console.WriteLine("4. SORT the list;");
+            Console.WriteLine("5. SEARCH the list;");
+            Console.WriteLine("6. Print the HELP menu;");
+            Console.WriteLine("7. QUIT.");
             Console.WriteLine(new string('=', 30));
         }
 
         private static ComputerTable.Key GetKey()
         {
+            Console.Write(">>> ");
             ComputerTable.Key input;
-            while (!Enum.TryParse(Console.ReadLine(), true, out input))
+            while (!ProperParse(Console.ReadLine(), true, out input))
             {
                 Console.WriteLine("That's not a correct key.");
+                Console.Write(">>> ");
             }
             return input;
         }
-        
-        public static Item GetInput()
+
+        public static bool ProperParse<TEnum>(string value, bool ignoreCase, out TEnum result) where TEnum : struct
+        {
+            // Regular Enum.TryParse doesn't check if the parsed integer has an associated constant;
+            // This version merges parsing with definition checking.
+            return Enum.TryParse(value, ignoreCase, out result) && Enum.IsDefined(typeof(TEnum), result);
+        }
+
+        public static bool ProperParse<TEnum>(string value, out TEnum result) where TEnum : struct
+        {
+            return ProperParse(value, false, out result);
+        }
+
+        private static Item GetInput()
         {
             Console.Write(">>> ");
-            int result;
-            while (!Int32.TryParse(Console.ReadLine(), out result))
-                Console.WriteLine("Incorrect input, please enter an integer corresponding to your choice.");
+            Item selection;
+            while (!ProperParse(Console.ReadLine(), true, out selection))
+            {
+                Console.WriteLine("That's not a valid menu item.");
+                Console.Write(">>> ");
+            }
+            return selection;
+        }
 
-            return (Item)result;
+        private static bool GetConfirmation()
+        {
+            Console.Write(">>> ");
+            Confirmation result;
+            while (!ProperParse(Console.ReadLine(), true, out result))
+            {
+                Console.WriteLine("Failed to read bool value, try again.");
+                Console.Write(">>> ");
+            }
+
+            return (int) result == 1;
         }
     }
 }
--- Lab 1/Program.cs
+++ Lab 1/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using static Lab_1.Menu;
 
 namespace Lab_1
 {
@@ -7,40 +6,8 @@ namespace Lab_1
     {
         static void Main(string[] args)
         {
-            var list = new List<ComputerTable>();
-            PrintMenu();
-            bool running = true;
-            while (running)
-            {
-                switch (GetInput())
-                {
-                    case Menu.Item.Add :
-                        AddTable(list);
-                        break;
-                    case Menu.Item.Remove :
-                        RemoveTable(list);
-                        break;
-                    case Menu.Item.Print :
-                        PrintList(list);
-                        break;
-                    case Menu.Item.Sort :
-                        SortList(list);
-                        break;
-                    case Menu.Item.Search :
-                        SearchList(list);
-                        break;
-                    case Menu.Item.Help :
-                        PrintMenu();
-                        break;
-                    case Menu.Item.Quit :
-                        running = false;
-                        break;
-                    default:
-                        Console.WriteLine("That's not a valid command.");
-                        PrintMenu();
-                        break;
-                }
-            }
+            Menu.PrintMenu();
+            Menu.Run();
         }
     }
 }
