fed9be1454c89d2958a4f21f9263bb70b6fae849
farm-art <farmart@protonmail.com>
2020-03-05 23:10:02 +0300

    minor consistency cleanup and method rearrangement;

--- Lab 4/Menu.cs
+++ Lab 4/Menu.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 
 namespace Lab_4
 {
@@ -24,43 +23,49 @@ namespace Lab_4
             [Item.Quit] = () => _running = false
         };
 
-        public static void PrintHelp()
-        {
-            Console.WriteLine(new string('=', 30));
-            Console.WriteLine("What would you like to do?");
-            Console.WriteLine("1. ADD a new matrix to the list;");
-            Console.WriteLine("2. REMOVE an existing matrix from the list;");
-            Console.WriteLine("3. EDIT an existing matrix in the list;");
-            Console.WriteLine("4. PRINT all existing matrices;");
-            Console.WriteLine("5. CALCULATE a determinant;");
-            Console.WriteLine("6. Print the HELP menu again;");
-            Console.WriteLine("7. QUIT.");
-            Console.WriteLine(new string('=', 30));
-        }
-
         public static void InitArray()
         {
+            Console.WriteLine("Enter array length:");
+            Console.Write(">>> ");
             int input;
-            Console.Write("Enter array length: ");
             while (!Int32.TryParse(Console.ReadLine(), out input) || input < 1)
+            {
                 Console.WriteLine("The value entered is not an integer or isn't a valid array length.");
+                Console.Write(">>> ");
+            }
             SquareMatrix.InitArray(input);
         }
-        
+
         public static void AddMatrix()
         {
-            if (SquareMatrix.ArrLen <= SquareMatrix.ItemsAmount)
+            if (SquareMatrix.ItemsAmount >= SquareMatrix.ArrLen)
             {
                 Console.WriteLine("The array is full.");
                 return;
             }
-            Console.Write("Enter matrix size: ");
+            Console.WriteLine("Enter matrix size:");
+            Console.Write(">>> ");
             int input = 0;
             while (!Int32.TryParse(Console.ReadLine(), out input) ||
                    input < 1)
+            {
                 Console.WriteLine("Value entered is not an integer or a valid matrix size. Try again.");
+                Console.Write(">>> ");
+            }
             var mat = new SquareMatrix(input);
             SquareMatrix.AddItem(mat);
+        }
+
+        public static void RemoveMatrix()
+        {
+            if (SquareMatrix.ItemsAmount == 0)
+            {
+                Console.WriteLine("The array is empty.");
+                return;
+            }
+
+            Console.WriteLine("Enter index of matrix to remove.");
+            SquareMatrix.RemoveItem(GetIndex());
         }
 
         public static void EditMatrix()
@@ -70,19 +75,11 @@ namespace Lab_4
                 Console.WriteLine("The array is empty.");
                 return;
             }
+
+            Console.WriteLine("Enter index of matrix to edit.");
             var mat = SquareMatrix.GetItem(GetIndex());
             Console.WriteLine("Fill in the matrix:");
             mat.Input();
-        }
-
-        public static int GetIndex()
-        {
-            Console.WriteLine($"Enter index of item (0-{SquareMatrix.ItemsAmount - 1}):");
-            int input = -1;
-            while (!Int32.TryParse(Console.ReadLine(), out input) ||
-                   input < 0 || input >= SquareMatrix.ItemsAmount)
-                Console.WriteLine("The value entered is not an integer or isn't a valid index.");
-            return input;
         }
 
         public static void CalculateDeterminant()
@@ -98,26 +95,37 @@ namespace Lab_4
                 mat.Print();
                 Console.WriteLine($"The determinant of this matrix is: {mat.Determinant()}");
             }
-            else
-                Console.WriteLine("You'll quite literally never end up here.");
         }
 
-        public static void RemoveMatrix()
+        public static void PrintHelp()
         {
-            if (SquareMatrix.ItemsAmount == 0)
-            {
-                Console.WriteLine("The array is empty.");
-                return;
-            }
-            SquareMatrix.RemoveItem(GetIndex());
+            Console.WriteLine(new string('=', 30));
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("1. ADD a new matrix to the list;");
+            Console.WriteLine("2. REMOVE an existing matrix from the list;");
+            Console.WriteLine("3. EDIT an existing matrix in the list;");
+            Console.WriteLine("4. PRINT all existing matrices;");
+            Console.WriteLine("5. CALCULATE a determinant;");
+            Console.WriteLine("6. Print the HELP menu again;");
+            Console.WriteLine("7. QUIT.");
+            Console.WriteLine(new string('=', 30));
         }
 
-        public static Item GetInput()
+        public static void Run()
+        {
+            _running = true;
+            while (_running)
+            {
+                _options[GetInput()]();
+                Console.WriteLine(new string('=', 30));
+            }
+        }
+
+        private static Item GetInput()
         {
             Item input;
             Console.Write(">>> ");
-            while (!Enum.TryParse(Console.ReadLine(), true, out input) ||
-                   (int) input < 1 || (int) input > 7)
+            while (!ProperParse(Console.ReadLine(), true, out input))
             {
                 Console.WriteLine("That's not a valid menu item.");
                 Console.Write(">>> ");
@@ -126,11 +134,30 @@ namespace Lab_4
             return input;
         }
 
-        public static void Run()
+        private static int GetIndex()
         {
-            _running = true;
-            while (_running)
-                _options[GetInput()]();
+            Console.WriteLine($"Appropriate range: [0, {SquareMatrix.ItemsAmount - 1}]");
+            Console.Write(">>> ");
+            int input = -1;
+            while (!Int32.TryParse(Console.ReadLine(), out input) ||
+                   input < 0 || input >= SquareMatrix.ItemsAmount)
+            {
+                Console.WriteLine("The value entered is not an integer or isn't a valid index.");
+                Console.Write(">>> ");
+            }
+            return input;
+        }
+
+        public static bool ProperParse<TEnum>(string value, bool ignoreCase, out TEnum result) where TEnum : struct
+        {
+            // Regular Enum.TryParse doesn't check if the parsed integer has an associated constant;
+            // This version merges parsing with definition checking.
+            return Enum.TryParse(value, ignoreCase, out result) && Enum.IsDefined(typeof(TEnum), result);
+        }
+
+        public static bool ProperParse<TEnum>(string value, out TEnum result) where TEnum : struct
+        {
+            return ProperParse(value, false, out result);
         }
     }
 }
